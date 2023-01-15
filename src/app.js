85c84a7b251f1ca907826eaad163fc6b28bb4761
Ali  Chishti <alimuhammad@Alis-MacBook-Air-3.local>
2023-01-15 14:36:56 +0500

    complete automation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const puppeteer = require("puppeteer");
 
 function renderFile(filePath, fileType, res) {
   fs.readFile(filePath, function (error, data) {
@@ -18,15 +19,154 @@ function renderFile(filePath, fileType, res) {
   });
 }
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer(async function (req, res) {
   const path = url.parse(req.url).pathname;
   if (path === "/index.css") {
     renderFile(__dirname + "/index.css", "text/css", res);
   } else if (path === "/index.js") {
     renderFile(__dirname + "/index.js", "text/html", res);
   } else if (path === "/read-users") {
-    const filePath = __dirname.split("src")[0] + "data/voting_users.txt";
-    renderFile(filePath, "text/plain", res);
+    const usersFilePath = __dirname.split("src")[0] + "data/voting_users.txt";
+    const signedUsersFilePath =
+      __dirname.split("src")[0] + "data/already_signedup.txt";
+    const votedUsersFilePath =
+      __dirname.split("src")[0] + "data/already_voted.txt";
+
+    fs.readFile(usersFilePath, function (error, data) {
+      if (error) {
+        res.writeHead(404);
+        res.write(error);
+        res.end();
+      } else {
+        const usersText = data.toString().split("\n");
+        var users = [];
+        usersText.forEach((user) => {
+          user = user.split(", ");
+          users.push({
+            email: user[0],
+            name: user[1],
+            password: user[2],
+          });
+        });
+
+        fs.readFile(signedUsersFilePath, function (error, data) {
+          if (error) {
+            res.writeHead(404);
+            res.write(error);
+            res.end();
+          } else {
+            const signedUserEmails = data.toString().split("\n");
+            var processedUsers = users.map((user) => {
+              if (signedUserEmails.includes(user.email)) {
+                user.signedUp = true;
+              }
+              return user;
+            });
+
+            fs.readFile(votedUsersFilePath, function (error, data) {
+              if (error) {
+                res.writeHead(404);
+                res.write(error);
+                res.end();
+              } else {
+                const voteUserEmails = data.toString().split("\n");
+                processedUsers = processedUsers.filter(
+                  (user) => !voteUserEmails.includes(user.email)
+                );
+                res.write(JSON.stringify(processedUsers));
+                res.end();
+              }
+            });
+          }
+        });
+      }
+    });
+
+    // renderFile(filePath, "text/plain", res);
+  } else if (path === "/run-automation") {
+    req.on("data", async function (data) {
+      const users = JSON.parse(data.toString());
+
+      for (let user of users) {
+        const browser = await puppeteer.launch({
+          headless: false,
+          defaultViewport: null,
+          args: ["--start-maximized"],
+        });
+        const page = await browser.newPage();
+
+        // go to content creator page
+        await page.goto(
+          "https://creatordao.com/thesearch/profile/adam-elmasri"
+        );
+
+        // click vote button
+        await page.goto(
+          "https://creatordao.com/thesearch/profile/adam-elmasri"
+        );
+        const voteButtonSelector = "#recaptcha-btn";
+        await page.waitForSelector(voteButtonSelector, { visible: true });
+        await page.click(voteButtonSelector);
+
+        if (user.signedUp) {
+          // login
+          const loginButtonSelector = ".flex.justify-center.items-center";
+          await page.waitForSelector(loginButtonSelector, { visible: true });
+          await page.click(loginButtonSelector);
+        }
+
+        const emailSelector = "input[data-testid='email']";
+        await page.waitForSelector(emailSelector, { visible: true });
+        await page.type(emailSelector, user.email, { delay: 100 });
+
+        const submitButton = "button[data-testid='login-button']";
+        await page.waitForSelector(submitButton);
+        await page.click(submitButton);
+
+        if (!user.signedUp) {
+          const displayNameSelector = "input[name='display_name']";
+          await page.waitForSelector(displayNameSelector, { visible: true });
+          await page.type(displayNameSelector, user.name, { delay: 100 });
+        }
+
+        const passwordSelector = "input[name='password']";
+        await page.waitForSelector(passwordSelector, { visible: true });
+        await page.type(passwordSelector, user.password, { delay: 100 });
+
+        await page.waitForSelector(submitButton);
+        await page.click(submitButton);
+
+        await new Promise((r) => setTimeout(r, 10000));
+
+        if (!user.signedUp) {
+          const displayNameSelector = "input[name='display_name']";
+          await page.waitForSelector(displayNameSelector, { visible: true });
+          await page.type(displayNameSelector, user.name, { delay: 100 });
+
+          await page.waitForSelector(submitButton);
+          await page.click(submitButton);
+
+          fs.appendFileSync(
+            __dirname.split("src")[0] + "data/already_signedup.txt",
+            `${user.email}\n`
+          );
+          user.signedUp = true;
+        }
+
+        // vote button click
+        await page.waitForSelector(voteButtonSelector, { visible: true });
+        await page.click(voteButtonSelector);
+
+        // add in already voted list
+        fs.appendFileSync(
+          __dirname.split("src")[0] + "data/already_voted.txt",
+          `${user.email}\n`
+        );
+
+        // wait 2 minutes --> 2 x 60 x 1000 = 120,000
+        await new Promise((r) => setTimeout(r, 120000));
+      }
+    });
   } else {
     renderFile(__dirname + "/index.html", "text/html", res);
   }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@
   fetch("/read-users")
     .then((res) => res.text())
     .then((res) => {
-        const usersText = res.split("\n");
-        const users = [];
-        usersText.forEach(user => {
-            user = user.split(", ");
-            users.push({
-                email: user[0],
-                name: user[1],
-                password: user[2]
-            })
-        })
-        const userTable = document.getElementById("user-table");
+      localStorage.setItem("users", res);
+      const userTable = document.getElementById("user-table");
+      JSON.parse(res).forEach((user) => {
+        const tr = document.createElement("tr");
+
+        const td = document.createElement("td");
+        td.innerText = user.email;
+
+        tr.appendChild(td);
+
+        userTable.appendChild(tr);
+      });
     });
 })();
+
+function runAutomation() {
+  const users = localStorage.getItem("users");
+  fetch("/run-automation", {
+    method: "POST",
+    body: users,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
